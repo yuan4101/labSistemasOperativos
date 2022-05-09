f1336f6140d50077d799a181226e822b34ba418f
Juan Ante <juanantec@unicauca.edu.co>
2022-05-09 16:09:08 -0500

    c and makefile

---
+++ lab2/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int esDirectorio (char[]);
+
+int main(int argc, char *argv[]){
+    /* Codigo main de prueba    
+    printf("Busqueda de archivos\n");
+    esDirectorio("/home/juanantec/Documentos/nota.txt");
+    exit(EXIT_SUCCESS);
+    */
+
+
+}
+
+int esDirectorio(char *ruta){
+    struct stat buf;
+    
+    if (1)
+    {
+        /* code */
+    }
+    
+
+    /* Codigo de prueba
+    struct stat buf;
+    stat(ruta, &buf);
+    int size = buf.st_size;
+    printf("%d",size);
+
+    return 0;
+    */
+}
