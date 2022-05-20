e673d884f756a7eb87ea1cf31570f13c29a21e1a
darwinlatorre <darwilestib12@gmail.com>
2022-05-20 01:42:29 -0500

    Renamed file

---
+++ Busqueda_De_Archivo/README.md
@@ -0,0 +1,19 @@
+# Laboratorio 2 - Busqueda de archivos
+### Elaborado por:<br>Juan Manuel Ante (juanantec@unicauca.edu.co)<br>Darwin Estiben Latorre (delatorre@unicauca.edu.co)
+
+- Link GitHub: (`https://github.com/yuan4101/labSistemasOperativos/tree/main/lab2`)
+
+- Planteamiento del problema<br>
+Deberá desarrollar un programa que, mediante llamada a los servicios del sistema
+y las funciones de la biblioteca estándar de C, permita buscar un archivo (o
+directorio) dentro de una estructura de directorios del sistema. El programa deberá
+recibir por línea de comandos dos parámetros: El directorio base de la búsqueda,
+y el patrón (el texto) que debe encontrarse en el nombre del archivo o directorio.
+Algunas funciones de la biblioteca estándar de C y/o POSIX que se pueden usar son:<br>
+
+- stat
+- opendir
+- readdir
+- malloc
+- strstr
+- realpath
---
+++ Busqueda_De_Archivo/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+
+int esDirectorio (char[]);
+int buscar(char[], char[]);
+
+int main(int argc, char *argv[])
+{
+    printf("Busqueda de archivos\n\n");
+
+    if (argc != 3){
+        fprintf(stderr, "Ingrese un directorio y patron de busqueda!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Se encontraron '%d' resultados al buscar '%s' en la ruta '%s'\n", buscar(argv[1], argv[2]), argv[2], argv[1]);
+    
+    exit(EXIT_SUCCESS);
+}
+
+int esDirectorio(char *ruta){
+    struct stat buf;
+
+    if (stat(ruta, &buf) < 0){
+        // Los archivos corruptos generan una excepcion de stat, se opta por comentar la siguiente linea
+        //perror("stat");
+        return 0;
+    }
+
+    if (S_ISDIR(buf.st_mode)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int buscar(char *directorio, char *patron){
+    int total = 0;
+    char * ruta = (char*) malloc(300 * sizeof(char));
+    struct dirent *ent;
+    DIR *dir = opendir(directorio);
+
+    if (dir == NULL)
+    {
+        perror("opendir");
+        return total;
+    }
+    
+    while ((ent = readdir(dir)) != NULL){
+        strcpy(ruta, directorio);
+        strcat(ruta, "/");
+        strcat(ruta, ent->d_name);
+
+        if (strstr(ent->d_name, patron) != NULL){
+            printf("- %s\n", ruta);
+            total = total + 1;
+        }
+
+        if (esDirectorio(ruta) && strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0){
+            total = total + buscar(ruta, patron);
+        }
+    }
+
+    free(ruta);
+
+    closedir(dir);
+
+    return total;
+}
